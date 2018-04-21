3b9caddf5ef4a59cf748aa765519ab2cceb5079c
Matthew-Burfield <m1.burfield@gmail.com>
2018-04-21 10:56:58 +1000

    updated port for heroku

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.get("/currentroster", async function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log(`Example app listening on port ${PORT}!`);
 });
