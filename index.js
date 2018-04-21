6468b35e9b5f9f2df21632701ac29e51d2c595fe
Matthew-Burfield <m1.burfield@gmail.com>
2018-04-21 12:29:27 +1000

    logging timezone

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.get("/mynextschedule", async function(req, res) {
     });
   const nextShiftStart = moment(scheduleInfo[0].start * 1000).utc();
   const nextShiftFinish = moment(scheduleInfo[0].finish * 1000).utc();
+  console.log(nextShiftStart.zone());
   res.json({
     messages: [
       {
