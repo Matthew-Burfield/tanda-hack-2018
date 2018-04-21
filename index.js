16f4c40263395a5df1bf8d4dbace00619f1d0eca
Matthew-Burfield <m1.burfield@gmail.com>
2018-04-21 11:50:55 +1000

    added mynextschedule route

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
 var express = require("express");
 var axios = require("axios");
+var moment = require("moment");
 var app = express();
 
 const token =
   "b4852c73abee95c3be632ac78e2edaa71cf95d7dba41ee6a267164645fd6b630";
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+const employeeId = "570562";
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
 });
 
-app.get("/currentroster", async function(req, res) {
+// app.get("/currentroster", async function(req, res) {
+//   console.log(req.query);
+//   const rosterInfo = await axios
+//     .get("https://my.tanda.co/api/v2/rosters/current?show_costs=true")
+//     .then(response => response.data)
+//     .catch(err => {
+//       //silently fail like a ninja
+//     });
+//   res.json({
+//     messages: [
+//       {
+//         text: `Current roster starts on ${rosterInfo.start} and finishes on ${
+//           rosterInfo.finish
+//         }`
+//       },
+//       {
+//         text: `There are ${
+//           rosterInfo.schedules.length
+//         } schedules on this roster at a cost of ${rosterInfo.cost}`
+//       }
+//     ]
+//   });
+// });
+
+app.get("/mynextschedule", async function(req, res) {
   console.log(req.query);
-  const rosterInfo = await axios
-    .get("https://my.tanda.co/api/v2/rosters/current?show_costs=true")
+  const today = moment().format("YYYY-MM-DD");
+  const sevenDaysFromToday = moment()
+    .add(7, "days")
+    .format("YYYY-MM-DD");
+  const scheduleInfo = await axios
+    .get(
+      `https://my.tanda.co/api/v2/schedules?user_ids=${employeeId}&from=${today}&to=${sevenDaysFromToday}&show_costs=true&include_names=false`
+    )
     .then(response => response.data)
     .catch(err => {
       //silently fail like a ninja
     });
+  const nextShiftStart = moment(scheduleInfo[0].start * 1000);
+  const nextShiftFinish = moment(scheduleInfo[0].finish * 1000);
   res.json({
     messages: [
       {
-        text: `Current roster starts on ${rosterInfo.start} and finishes on ${
-          rosterInfo.finish
-        }`
-      },
-      {
-        text: `There are ${
-          rosterInfo.schedules.length
-        } schedules on this roster at a cost of ${rosterInfo.cost}`
+        text: `Your next shift is on ${nextShiftStart.format(
+          "dddd"
+        )} from ${nextShiftStart.format("h:mma")} to ${nextShiftFinish.format(
+          "h:mma"
+        )}`
       }
     ]
   });
